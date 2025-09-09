0e3bb590d6ed101f6dbbea1e70706366ddd615d0
Gr0wMan <bletsdone@gmail.com>
2025-09-09 21:51:54 +0300

    import

---
+++ src/client/java/me/yuugao/meyuugaosradio/client/RadioClient.java
@@ -0,0 +1,10 @@
+package me.yuugao.meyuugaosradio.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadioClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/yuugao/meyuugaosradio/Radio.java
@@ -0,0 +1,10 @@
+package me.yuugao.meyuugaosradio;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Radio implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
