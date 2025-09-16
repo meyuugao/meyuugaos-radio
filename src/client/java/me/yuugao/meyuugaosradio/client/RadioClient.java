00336255ec361b193e65d919e7d60fd86902fe03
Gr0wMan <bletsdone@gmail.com>
2025-09-16 07:50:06 +0300

    1.18

---
+++ src/client/java/me/yuugao/meyuugaosradio/client/RadioClient.java
@@ -0,0 +1,21 @@
+package me.yuugao.meyuugaosradio.client;
+
+import static me.yuugao.meyuugaosradio.Constants.CLIENT_LOGGER;
+
+
+import me.yuugao.meyuugaosradio.client.config.ClientModConfigManager;
+import me.yuugao.meyuugaosradio.client.events.ClientEventsManager;
+import me.yuugao.meyuugaosradio.client.network.ClientNetworkManager;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadioClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientModConfigManager.initialize();
+        ClientNetworkManager.initialize();
+        ClientEventsManager.initialize();
+
+        CLIENT_LOGGER.info("MeYuugaos Radio mod initialized!");
+    }
+}
