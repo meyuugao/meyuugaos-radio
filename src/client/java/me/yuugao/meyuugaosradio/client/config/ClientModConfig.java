892524237a5bf1ac7a8e8bf463c3dff14bbc46cd
MeYuugao <meyuugao@gmail.com>
2026-02-23 20:37:25 +0300

    client1

--- src/client/java/me/yuugao/meyuugaosradio/client/config/ClientModConfig.java
+++ src/client/java/me/yuugao/meyuugaosradio/client/config/ClientModConfig.java
@@ -3,9 +3,21 @@ package me.yuugao.meyuugaosradio.client.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.ConfigData.ValidationException;
 
 @Config(name = "meyuugaosradio_client")
 public class ClientModConfig implements ConfigData {
-    @ConfigEntry.BoundedDiscrete(min = 0, max = 100)
     public int volume = 100;
+
+    @ConfigEntry.Gui.Tooltip
+    public int audioBufferSize = 1000;
+
+    @Override
+    public void validatePostLoad() {
+        if (volume < 0) volume = 0;
+        if (volume > 100) volume = 100;
+
+        if (audioBufferSize < 100) audioBufferSize = 100;
+        if (audioBufferSize > 2000) audioBufferSize = 2000;
+    }
 }
